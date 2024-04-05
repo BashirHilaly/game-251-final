0a7de79d3259f074b5f6156081652f3dbc607a55
Hilaly <bvh5512@psu.edu>
2024-04-05 10:19:51 -0400

    Created file structure and first MainMenu scene

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Phaser from 'phaser';
 
+import MainMenu from './scenes/MainMenu';
+
+// To run server, "npm start"
+
 const config = {
-    width: 800,
-    height: 500,
+    width: 1280,
+    height: 720,
     type: Phaser.AUTO
 }
 
 const game = new Phaser.Game(config);
+
+game.scene.add('MainMenu', MainMenu);
+
+game.scene.start('MainMenu');
